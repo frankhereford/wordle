5246758d21e4d436b8fd3d4f9512210ebd288ee7
Frank Hereford <frankhereford@users.noreply.github.com>
2022-01-30 12:28:36 -0600

    Test another change

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,12 +9,12 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source clone of the game Wordle -{' '}
+        This is an open source clone of the game Wordle.{' '}
         <a
-          href="https://github.com/hannahcode/wordle"
+          href="https://github.com/frankhereford/wordle"
           className="underline font-bold"
         >
-          check out the code here
+          Fork of code here
         </a>{' '}
         and{' '}
         <a
@@ -23,6 +23,10 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
         >
           play the original here
         </a>
+        .
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        This build includes mods to reduce guesses to five and reset every hour.
       </p>
     </BaseModal>
   )
